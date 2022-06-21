9a5f6e69dd3de502882a674b4cd8399c375a5ccb
MarielBoher <mariellboheer@gmail.com>
2022-06-21 15:55:15 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { IngresoComponent } from './ingreso/ingreso.component';
+import { EgresoComponent } from './egreso/egreso.component';
+import { CabeceroComponent } from './cabecero/cabecero.component';
+import { IngresoServicio } from './ingreso/ingreso.servicio';
+import { EgresoServicio } from './egreso/egreso.servicio';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormularioComponent,
+    IngresoComponent,
+    EgresoComponent,
+    CabeceroComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [IngresoServicio, EgresoServicio],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
